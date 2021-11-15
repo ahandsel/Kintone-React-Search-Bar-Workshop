86d989f4bade829b9a141b8bcebb5a48a1a94c0d
Genji Fujimori <30670749+ahandsel@users.noreply.github.com>
2021-11-16 04:40:04 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    KintoneCustomization: path.join(__dirname, 'src', 'index.js'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader', // https://webpack.js.org/loaders/babel-loader/#root
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'] // https://babeljs.io/docs/en/presets/
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ]
+
+};
