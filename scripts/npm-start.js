91a412542b857ae9ba63622901fb90dc90726907
ahandsel <ahandsel@gmail.com>
2021-11-17 10:50:34 +0900

    Update npm-start.js

--- scripts/npm-start.js
+++ scripts/npm-start.js
@@ -3,13 +3,25 @@ const runAll = require('npm-run-all');
 
 runAll(['dev', 'upload'], {
   parallel: true,
-  stdout: process.stdout,
-  stdin: process.stdin
+  stdout: process.stdout, // Pushes printed output of each process to the terminal
+  stdin: process.stdin // Kintone Customize uploader may ask for login info
 }).catch(({results}) => {
   results
-    .filter(({code}) => code)
+    .filter(({code}) => { // filter for code: 1
+      console.log({code});
+      return code;
+    })
     .forEach(({name}) => {
       console.log(`"npm run ${name}" failed`);
     })
   ;
+  console.log(results);
 });
+
+/* Example of results object
+{
+  name: undefined,
+  code: 1,
+  results: [ { name: 'dev', code: undefined }, { name: 'upload', code: 1 } ]
+}
+*/
